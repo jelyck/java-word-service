0b78c65dea4bd410144bec3978940a5364c238b8
jelyck <jenslycke@gmail.com>
2023-09-26 21:39:09 +0200

    initial springboot application

---
+++ src/main/java/io/jelyck/javawordservice/JavaWordServiceApplication.java
@@ -0,0 +1,13 @@
+package io.jelyck.javawordservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWordServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jelyck/javawordservice/JavaWordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jelyck.javawordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
